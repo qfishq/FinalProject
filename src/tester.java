8b454a96a598abcb9341ab016a1d48ce514ea407
qfishq <559463@my.cuhsd.org>
2024-12-02 12:04:54 -0800

    ARGGHHHH someone pls override the comparable interface for me pls i cant do this

--- src/tester.java
+++ src/tester.java
@@ -2,6 +2,9 @@ package src;
 
 public class tester {
     public static void main (String[] args) {
-
+            TodoList kimmy = new TodoList();
+            kimmy.Add("03/12/23", "hide the trash");
+            kimmy.Add("02/15/34", "eat rabbit");
+            System.out.println(kimmy);
         }
 }
