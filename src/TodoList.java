569b8b6bb1211bf8ff3bebe1c218e24afa1249eb
qfishq <559463@my.cuhsd.org>
2024-12-02 11:30:13 -0800

    added empty tester

--- src/TodoList.java
+++ src/TodoList.java
@@ -4,22 +4,26 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class TodoList {
-    //replace first String with Item later
+    //replace second String with Item later
+    /**key = date, value = item**/
     private Map<String, String> todolist;
     public TodoList(){
         todolist = new TreeMap<>();
     }
-    //replace first String with Item later
+    //replace second String with Item later
     public void Add(String k, String v) {
         todolist.put(k, v);
     }
-    //replace first String with Item later
+    //replace second String with Item later
     public void Remove(String k, String v) {
         todolist.remove(k, v);
     }
 
     public String toString() {
+        String res = "";
+        for (int i = 0; i < todolist.size(); i++) {
 
+        }
         return super.toString();
     }
 }
---
+++ src/tester.java
@@ -0,0 +1,7 @@
+package src;
+
+public class tester {
+    public static void main (String[] args) {
+
+        }
+}
