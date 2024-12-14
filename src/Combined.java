28213f34aa477a99e16606298c5650ef2f10653b
AmeyaKezhukkettu <559514@my.cuhsd.org>
2024-12-14 14:17:56 -0800

    Combined everything, it should work! However we might want to display more info about tasks, but tbh it should be good

--- src/Combined.java
+++ src/Combined.java
@@ -5,15 +5,71 @@ public class Combined {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         GoalList gl = new GoalList();
+        TodoList todos = new TodoList();
         int action;
 
         do {
-            System.out.println("Welcome to the Life Organizer! Type in a number to continue!\n(1) Add to you todo list\n(2) Update your todo list\n(3) Add to your goals\n(4) Update your goals\n(5) View your lists \n(6) Quit");
+            System.out.println("Type in a number to continue with Life Organizer!\n(1) Add to you todo list\n(2) Update your todo list\n(3) Add to your goals\n(4) Update your goals\n(5) View your lists \n(6) Quit");
 
             action = input.nextInt();
             input.nextLine();
             if(action == 1){
 
+                System.out.println(todos.toString());
+                Item fred;
+
+                System.out.println("What is the ToDo's name:");
+                String name = input.nextLine();
+
+                System.out.println("This is an urgent TODO (True/False):");
+                Boolean urgent = input.nextBoolean();
+                input.nextLine();
+
+                System.out.println("When is the due date (dd/mm):");
+                String duedate = input.nextLine();
+
+                System.out.println("How much time does it take to complete (hours):");
+                int time = input.nextInt();
+                input.nextLine();
+
+                System.out.println("Is you ToDo an assignment(a), or personal event(p): ");
+                String response = input.nextLine();
+
+                if(response.equals("a")){
+                    System.out.println("How many points is it worth:");
+                    int points = input.nextInt();
+                    input.nextLine();
+
+                    System.out.println("What subject is it for: ");
+                    String subject = input.nextLine();
+                    fred = new Assignment(name, duedate, duedate, points, subject);
+                }
+                else if(response.equals("p")){
+                    System.out.println("Where is the location:");
+                    String location = input.nextLine();
+
+                    System.out.println("additional info:");
+                    String addInfo = input.nextLine();
+                    fred = new PersonalEvent(name, urgent, duedate, location, addInfo);
+                }
+
+                //Here we add the fred to the TodoList (need to modify the class)
+                todos.Add(duedate, name);
+
+                System.out.println("Updated Todo List:");
+                System.out.println(todos.toString());
+
+            }
+            else if(action == 2){
+                System.out.println(todos.toString());
+                System.out.println();
+                System.out.println("Which task have you completed (enter the date)");
+                String dateToRemove = input.nextLine();
+
+                todos.Remove(dateToRemove);
+
+                System.out.println("Updated Todo List:");
+                System.out.println(todos.toString());
             }
 
             else if(action == 3) {
@@ -74,6 +130,15 @@ public class Combined {
                         break;
                     }
                 }
+            }
+            else if(action == 5){
+
+                //We'll put the todoList before
+                System.out.println("GOALS:");
+                gl.view();
+                System.out.println("\nTODOs:");
+                System.out.println(todos.toString());
+
             }
         }
         while(action != 6);
